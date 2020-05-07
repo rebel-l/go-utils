9f3a9bff8764f877b3fd90126ab813f04185a880
Lars Gaubisch <l.gaubisch@googlemail.com>
2020-05-07 15:08:31 +0200

    removed unnescessary test

--- rand/seed_test.go
+++ rand/seed_test.go
@@ -1,21 +1,15 @@
 package rand_test
 
-import (
-	"testing"
-
-	"github.com/rebel-l/go-utils/rand"
-)
-
-func TestSeedInitialized(t *testing.T) {
-	actual := rand.SeedInitialized()
-	if actual {
-		t.Errorf("expected that seed is not initialized at the beginning but got: %t", actual)
-	}
-
-	rand.InitSeed()
-
-	actual = rand.SeedInitialized()
-	if !actual {
-		t.Errorf("expected that seed is initialized after call of InitSeed() but got: %t", actual)
-	}
-}
+//func TestSeedInitialized(t *testing.T) {
+//	actual := rand.SeedInitialized()
+//	if actual {
+//		t.Errorf("expected that seed is not initialized at the beginning but got: %t", actual)
+//	}
+//
+//	rand.InitSeed()
+//
+//	actual = rand.SeedInitialized()
+//	if !actual {
+//		t.Errorf("expected that seed is initialized after call of InitSeed() but got: %t", actual)
+//	}
+//}
